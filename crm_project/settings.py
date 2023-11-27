23f3d3874c3d04561d1bbbcd942550c18885cbce
G Chase Gravett <102496663+chasegravett@users.noreply.github.com>
2023-11-26 18:41:24 -0600

    Update settings.py

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'crmdb',
         'USER': 'postgres',
-        'PASSWORD': 'Pickledpickles1!',
+        'PASSWORD': '***********',
         'HOST': 'localhost',
         'PORT': 5432,
     }
